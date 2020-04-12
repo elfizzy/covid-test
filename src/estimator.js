4672fe12ee65db59c24e2199b0fe6f48bd9d2774
elfizzy <abulkhair0800@gmail.com>
2020-04-12 21:14:14 +0100

    changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,3 @@
-const pandemicData = {
-    region: {
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-
-  };
 const covid19ImpactEstimator = (data) => {
   const output = {
     data,
