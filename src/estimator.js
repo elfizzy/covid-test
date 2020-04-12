c9a7ff707dafa3962d477831469ab18eb3f99f4c
elfizzy <abulkhair0800@gmail.com>
2020-04-12 09:15:11 +0100

    new commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    dat= {
+      region: {
+          avgAge: 19.7,
+          avgDailyIncomeInUSD: 5,
+          avgDailyIncomePopulation: 0.71
+          },
+          periodType: "days",
+          timeToElapse: 58,
+          reportedCases: 674,
+          population: 66622705,
+          totalHospitalBeds: 1380614
+
+  }
+
+  };
 
 export default covid19ImpactEstimator;
