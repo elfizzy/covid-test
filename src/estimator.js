b4b8042671a3e8bd2f5280acf8cae6817e32bf54
elfizzy <abulkhair0800@gmail.com>
2020-04-12 09:59:57 +0100

    solved issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,17 @@
 const covid19ImpactEstimator = (data) => {
-    dat= {
-      region: {
-          avgAge: 19.7,
-          avgDailyIncomeInUSD: 5,
-          avgDailyIncomePopulation: 0.71
-          },
-          periodType: "days",
-          timeToElapse: 58,
-          reportedCases: 674,
-          population: 66622705,
-          totalHospitalBeds: 1380614
-
-  }
+  data = {
+    region: {
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
 
   };
+};
 
 export default covid19ImpactEstimator;
