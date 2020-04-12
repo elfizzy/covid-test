70097bffc4aa1451b7e7d97406261e14bb89f846
elfizzy <abulkhair0800@gmail.com>
2020-04-12 21:23:55 +0100

    make new changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,4 @@
 const covid19ImpactEstimator = (data) => {
-  const output = {
-    data,
-    estimate: {
-      impact: {},
-      severeImpact: {}
-    }
-  }
 };
 
 export default covid19ImpactEstimator;
